90081184aaf3c86c22cbfcac4dc54add38f8cc9f
JeonghyeonK <6843wjdgus@naver.com>
2023-07-10 22:38:47 +0900

    대기업 코테 유형 모음 22/83

--- 대기업 코테 유형 모음/19941.py
+++ 대기업 코테 유형 모음/19941.py
@@ -1,3 +1,16 @@
 N, K = map(int, input().split())
-print(N)
-print(K)
+str = input()
+
+lastH = -1
+result = 0
+for i in range(N):
+    if str[i] == 'P':
+        mostLeft = max(0, i-K, lastH)
+        mostRight = min(N-1, i+K)
+        for j in range(mostLeft, mostRight+1):
+            if str[j] == 'H' and j != lastH:
+                lastH = j
+                result += 1
+                break   
+
+print(result)
