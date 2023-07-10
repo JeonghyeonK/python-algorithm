d26f06c5d253f6ed76a1cf3fb3ec35b7ae2f49de
JeonghyeonK <6843wjdgus@naver.com>
2023-07-10 21:55:44 +0900

    대기업 코테 유형 모음 21/83

--- 대기업 코테 유형 모음/1515.py
+++ 대기업 코테 유형 모음/1515.py
@@ -3,11 +3,12 @@ strs = input()
 num = 0
 while len(strs)>0:
     num += 1
-    if strs[0] in str(num) :
-        
-        strs = strs[1:]
-        
-        
-        
+    numstr = str(num)
+    
+    for i in range(len(numstr)):
+        if strs[0] == numstr[i] :
+            strs = strs[1:]
+            if len(strs) == 0 :
+                break
     
 print(num)
