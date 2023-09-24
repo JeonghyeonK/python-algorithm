fdbe051c722853c7dba05aa00df4c5253c025376
JeonghyeonK <6843wjdgus@naver.com>
2023-09-24 23:09:21 +0900

    1912 - 정답 참고

--- 기타/1912.py
+++ 기타/1912.py
@@ -1,35 +1,43 @@
-import sys
-from collections import deque
-input = sys.stdin.readline
-
 n = int(input())
-num_list = list(map(int, input().split()))
-
-pnm = deque()
-max_num = -1001
-
-for i in num_list:
-    if max_num <= 0:
-        max_num = max(max_num, i)
+m = list( map(int, input().split(' ')))
+ 
+for i in range(1, n):
+    m[i] = max(m[i], m[i] + m[i-1])
     
-    if len(pnm) == 0:
-        pnm.append(i)
-    elif pnm[-1] * i >=0:
-        pnm[-1] += i
-    else:
-        pnm.append(i)
+print(max(m))
 
-print(pnm)
+# import sys
+# from collections import deque
+# input = sys.stdin.readline
 
-if max_num <= 0:
-    print(max_num)
-else:
-    for i in range(len(pnm) - 2):
-        if pnm[i] <= 0:
-            continue
-        if pnm[i] >= -pnm[i+1] and pnm[i+2] >= -pnm[i+1]:
-            pnm[i+2] = pnm[i] + pnm[i+1] + pnm[i+2]
-            i += 1
+# n = int(input())
+# num_list = list(map(int, input().split()))
 
-    print(pnm)
-    print(max(pnm))
+# pnm = deque()
+# max_num = -1001
+
+# for i in num_list:
+#     if max_num <= 0:
+#         max_num = max(max_num, i)
+    
+#     if len(pnm) == 0:
+#         pnm.append(i)
+#     elif pnm[-1] * i >=0:
+#         pnm[-1] += i
+#     else:
+#         pnm.append(i)
+
+# print(pnm)
+
+# if max_num <= 0:
+#     print(max_num)
+# else:
+#     for i in range(len(pnm) - 2):
+#         if pnm[i] <= 0:
+#             continue
+#         if pnm[i] >= -pnm[i+1] and pnm[i+2] >= -pnm[i+1]:
+#             pnm[i+2] = pnm[i] + pnm[i+1] + pnm[i+2]
+#             i += 1
+
+#     print(pnm)
+#     print(max(pnm))
