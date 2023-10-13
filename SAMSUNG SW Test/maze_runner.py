58c688e1542d61f32151eae1f82bded2b9e95141
JeonghyeonK <6843wjdgus@naver.com>
2023-10-13 23:29:19 +0900

    삼성 SW Test 대비 - 메이즈러너 - 재도전 선언

--- SAMSUNG SW Test/maze_runner.py
+++ SAMSUNG SW Test/maze_runner.py
@@ -1,39 +1,41 @@
 #포기
+while True:
+    N, M, K = map(int, input().split())
 
-N, M, K = map(int, input().split())
+    miroh = list()
+    miroh.append([0 for _ in range(N+1)])
+    for _ in range(N):
+        miroh.append([0] + list(input().split()))
 
-miroh = list()
-miroh.append([0 for _ in range(N+1)])
-for _ in range(N):
-    miroh.append([0] + list(input().split()))
+    people = list()
+    for _ in range(M):
+        people.append(list(map(int, input().split())))
 
-people = list()
-for _ in range(M):
-    people.append(list(map(int, input().split())))
+    exit = list(map(int, input().split()))
 
-exit = list(map(int, input().split()))
+    print(people)
 
-print(people)
+    for _ in range(K):
+        for man in people:
+            next_sqaure = man.copy()
+            #print(next_sqaure)
+            if man[0] > exit[0]:
+                next_sqaure[0] -= 1
+            elif man[0] < exit[0]:
+                next_sqaure[0] += 1
+            if miroh[next_sqaure[0]][next_sqaure[1]] == 0:
+                man = next_sqaure.copy()
+                continue
 
-for _ in range(K):
-    for man in people:
-        next_sqaure = man.copy()
-        #print(next_sqaure)
-        if man[0] > exit[0]:
-            next_sqaure[0] -= 1
-        elif man[0] < exit[0]:
-            next_sqaure[0] += 1
-        if miroh[next_sqaure[0]][next_sqaure[1]] == 0:
-            man = next_sqaure.copy()
-            continue
-
-        if man[1] > exit[1]:
-            next_sqaure[1] -= 1
-        elif man[1] < exit[1]:
-            next_sqaure[1] += 1
-        if miroh[next_sqaure[0]][next_sqaure[1]] == 0:
-            man = next_sqaure.copy()
-            continue
-print(people)
+            if man[1] > exit[1]:
+                next_sqaure[1] -= 1
+            elif man[1] < exit[1]:
+                next_sqaure[1] += 1
+            if miroh[next_sqaure[0]][next_sqaure[1]] == 0:
+                man = next_sqaure.copy()
+                continue
+    print(people)
+    break
             
-        
+#포기 취소
+#재도전        
